9f815be53167578a824c133e857a92fe76b9de67
krishnaa-14 <68679770+krishnaa-14@users.noreply.github.com>
2023-04-30 16:31:41 +0530

    JUST MODIFIED HEADER WITH A ICON

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className = "container my-5 text-center">
-            <h1> TODO - LIST</h1>
+        <div className="container my-5 text-center">
+            <h1 className="display-4 fw-bold">TO-DO LIST 😎</h1>
         </div>
     )
 }
